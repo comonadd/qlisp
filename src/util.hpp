a74da96fbb9fae28fc9a61e0ed32c57d907ab50f
Dmitry Guzeev <dmitri.guzeev@gmail.com>
2021-09-06 14:32:07 +0300

    Exit on assertion error

--- src/util.hpp
+++ src/util.hpp
@@ -1,10 +1,13 @@
 #ifndef UTIL_HPP
 #define UTIL_HPP
 
+#include <stdlib.h>
+
 #if DEBUG
 inline void assert_stmt(bool expr, std::string msg) {
   if (!expr) {
     printf("Assertion failed: %s\n", msg.c_str());
+    exit(1);
   }
 }
 #else
